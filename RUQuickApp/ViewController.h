260a7e8e7fc590089490a5e1d033cf9d11b442e4
aadesh <aadeshp95@gmail.com>
2015-03-28 22:37:22 -0400

    Initial Commit

---
+++ RUQuickApp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  RUQuickApp
+//
+//  Created by Aadesh Patel on 3/28/15.
+//  Copyright (c) 2015 Aadesh Patel. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ RUQuickAppTests/RUQuickAppTests.m
@@ -0,0 +1,40 @@
+//
+//  RUQuickAppTests.m
+//  RUQuickAppTests
+//
+//  Created by Aadesh Patel on 3/28/15.
+//  Copyright (c) 2015 Aadesh Patel. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface RUQuickAppTests : XCTestCase
+
+@end
+
+@implementation RUQuickAppTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
